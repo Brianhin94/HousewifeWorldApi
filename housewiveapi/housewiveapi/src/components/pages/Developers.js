1e50e14abfece29a95b7883cf940d5293e45c78a
Brianhin94 <brianhin64@gmail.com>
2021-02-23 15:19:23 -0700

    half

--- housewiveapi/housewiveapi/src/components/pages/Developers.js
+++ housewiveapi/housewiveapi/src/components/pages/Developers.js
@@ -5,7 +5,7 @@ const Developers = (props) => {
             <div className="meet-devs">
                 <h3><span>Katie Mackenbrock</span></h3>
                 <h3><span>Ian Jacobs</span></h3>
-                <h3><span>Brian Hinchey</span></h3>
+                <h3><span>Brian Derry Hinchey</span></h3>
             </div>
         </div>
     );
